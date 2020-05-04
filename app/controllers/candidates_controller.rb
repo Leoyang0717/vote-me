c3b3d6142b95e69635490365002e6849dcfccc7b
Leoyang0717 <pithend58639@gmail.com>
2020-05-04 22:54:26 +0800

    純手工打造003-005

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,5 +2,15 @@ class CandidatesController < ApplicationController
 
   def index
   end
-
+  def new
+    @candidate = Candidate.new
+  end
+  def create
+    @candidate = Candidate.new(params[:candidate])
+    if @candidate.save
+    redirect_to '/candidates'
+    else
+      #ng
+    end
+  end
 end

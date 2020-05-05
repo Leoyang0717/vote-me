84199d7c3f95e29dc11e196743577cbdec7128ba
Leoyang0717 <pithend58639@gmail.com>
2020-05-06 01:25:02 +0800

    純手工打造006-012

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,17 +1,44 @@
 class CandidatesController < ApplicationController
 
   def index
+    @candidates = Candidate.all
   end
   def new
     @candidate = Candidate.new
   end
   def create
-    @candidate = Candidate.new(params[:candidate])
+    @candidate = Candidate.new(candidate_params)
     if @candidate.save
     redirect_to '/candidates'
+    flash[:notice] = "Candidate created!"
     else
-      #ng
+      render :new #引導至 new.html.erb檔
     end
+  end
+  def show
+   @candidate = Candidate.find_by(id: params[:id])
+  end
+  def edit
+   @candidate = Candidate.find_by(id: params[:id])
+  end
+  def update
+  @candidate = Candidate.find_by(id: params[:id])
+  if @candidate.update(candidate_params)
+    redirect_to '/candidates'
+    flash[:notice] = "Candidate updated!"
+    else
+      render :edit #引導至 edit.html.erb檔
+    end
+  end
+  def destroy
+  @candidate = Candidate.find_by(id: params[:id])
+  @candidate.destroy
+  flash[:notice] = "Candidate deleted!"
+  redirect_to '/candidates'
+  end
 
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name ,:party, :age , :politics)
   end
 end
